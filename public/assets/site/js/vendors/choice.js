9d97ba65769f7a8962820b06250b02fbfe7381e6
xshmrz <xshmrz@gmail.com>
2025-06-11 10:10:18 +0300

    Update

--- public/assets/site/js/vendors/choice.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll("[data-choices]").forEach(function(e){new Choices(e,{removeItemButton:"true"===e.dataset.choicesRemoveitembutton,itemSelectText:"",searchEnabled:!1})})});
